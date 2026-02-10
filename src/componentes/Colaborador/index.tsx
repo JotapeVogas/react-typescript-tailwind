7c5337f57de3368c3e49737199bb12816f4770e3
JotapeVogas <joao.chaves@soulasalle.com.br>
2026-02-10 13:07:25 -0300

    corrigindo estilo da imagem no componente Colaborador

--- src/componentes/Colaborador/index.tsx
+++ src/componentes/Colaborador/index.tsx
@@ -13,7 +13,7 @@ interface ColaboradorProps {
 const styles = {
   container: "w-[280px]",
   cabecalho: "rounded-t-[10px] text-center flex flex-col items-end justify-center relative",
-  imagem: "w-[100px] rounded-full relative -bottom-[50px] block mx-auto",
+  imagem: "w-[100px] rounded-full relative mb-[-50px] block mx-auto",
   rodape: "bg-white shadow-[5px_5px_15px_rgba(0,0,0,0.08)] rounded-b-[10px] pt-[90px] pb-10",
   nome: "text-[#6278F7] text-lg leading-[22px] font-bold mb-2 px-4 justify-center text-center gap-10",
   texto: "text-lg leading-[22px] text-[#212121] px-4 justify-center text-center gap-10",
